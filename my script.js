0399e5d8a5bc12de5b8a5510ce1d71f54cc54f12
Somaiyanoori <164001610+Somaiyanoori@users.noreply.github.com>
2024-05-20 18:37:19 +0430

    Add files via upload

---
+++ my script.js
@@ -0,0 +1,27 @@
+// 1.
+let city = "Herat";
+let region = "Herat Province"; 
+let populationDensity = 724.172 ;
+let area = 54.778; 
+
+// 2. Log values to the console
+console.log("City:", city);
+console.log("Region:", region);
+console.log("Population Density:", populationDensity, "people per square kilometer");
+console.log("Area:", area, "square kilometers");
+
+// 3.
+let isCapital = false; // Herat is not the capital
+let officialLanguage; 
+
+// 4. 
+console.log("Type of isCapital:", typeof isCapital);//because of false is a Boolean
+console.log("Type of populationDensity:", typeof populationDensity);//because i wrote number of population
+console.log("Type of city:", typeof city); //because i wrote Herat at this ==> ""
+console.log("Type of officialLanguage:", typeof officialLanguage); //because i did not write a value 
+
+
+
+
+
+
